4be57f2917a72c7bc4b028127be3077efd8ea9cc
Chat Wacharamanotham <chat@ifi.uzh.ch>
2020-10-28 05:47:15 +0100

    Add JSON output

--- R/generate_agora.R
+++ R/generate_agora.R
@@ -19,13 +19,17 @@ coder2_codes <-
   read_lines(path_coder1) %>% 
   setdiff(agreed_codes)
 
-
-bind_rows(
+# merge codes
+codes_df <- 
+  bind_rows(
   tibble(code = coder1_codes, coder = coder1_name),
   tibble(code = coder2_codes, coder = coder2_name),
   tibble(code = agreed_codes, coder = "Agreed")) %>% 
   arrange(code) %>% 
-  mutate(line_no = 1:n()) %>% 
+  mutate(line_no = 1:n()) 
+
+# create CSV and show output path
+codes_df %>% 
   mutate(
     "{{coder1_name}}'s trash" := "",
     "{{coder1_name}}" := if_else(coder == coder1_name, code, ""),
@@ -36,3 +40,15 @@ bind_rows(
   write_excel_csv(path_output)
 
 system2("open", paste0("\"", dirname(normalizePath(path_output)), "\""))
+
+# create JSON
+json_path <- file.path(dirname(normalizePath(path_output)), "agora.json")
+codes_df %>% 
+  unite(location, line_no, coder, sep = " - ") %>% 
+  mutate(code = str_replace_all(code, "\\\"", "\\\\\"")) %>% 
+  mutate(json = str_glue("\"{code}\": \"{location}\"")) %>% 
+  pull(json) %>% 
+  paste(collapse = ",\n") %>% 
+  paste0("{", ., "}") %>% 
+  write_file(json_path)
+  
